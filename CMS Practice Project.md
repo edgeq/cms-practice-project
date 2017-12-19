8bf35dfa5234f2f366d86c4d1204a9c13f1f36d3
Edge Quintanilla <edgarq86@gmail.com>
2017-12-19 15:08:03 -0600

    first commit of cms project

---
+++ CMS Practice Project.md
@@ -0,0 +1,5 @@
+# CMS Practice Project
+
+This is a project that walks through the creation of a Content Management System. It is intended as a learning exercise. 
+
+Original course content can be found on [Lynda.com - PHP & MySQL Training](https://www.lynda.com/PHP-tutorials/PHP-MySQL-Essential-Training-1-Basics/587674-2.html)
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CMS Practice Project</title>
+</head>
+<body>
+  <h1><code>Welcome to Root</code></h1>
+  <h2>This is  a project that will use the MVC paradigm while building a CMS.</h2>
+
+</body>
+</html>
