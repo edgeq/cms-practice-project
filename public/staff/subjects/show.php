fedb43a7bf402626bbd14702970bbda3ee46d975
Edge Quintanilla <edgarq86@gmail.com>
2017-12-19 20:21:45 -0600

    discussed url encodes and security

---
+++ public/staff/subjects/show.php
@@ -0,0 +1,12 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php //$id=$_GET['id'];
+
+$id = isset($_GET['id']) ? $_GET['id'] : '1';
+
+echo h($id);
+ ?>
+
+<a href="show.php?name=<?php echo u('John Doe');?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More');?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?');?>">Link</a><br />
